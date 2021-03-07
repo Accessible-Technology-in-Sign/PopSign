09e84b89ea4763b7189c1b35f55c83025219b3db
ConnorPeng <pjh_jinghong@outlook.com>
2021-03-07 16:56:01 -0500

    solved #5
    solved #5

--- Assets/PopSignMain/Scripts/Bubbles/ball.cs
+++ Assets/PopSignMain/Scripts/Bubbles/ball.cs
@@ -387,7 +387,7 @@ public class ball : MonoBehaviour
     {
         if (PlayerPrefs.GetInt("OpenLevel") == 1 || PlayerPrefs.GetInt("OpenLevel") == 2)
         {
-            if ((mainscript.Instance.TopBorder.transform.position.y - transform.position.y <= 1.3 && LevelData.mode != ModeGame.Rounded) || (LevelData.mode == ModeGame.Rounded && tag == "chicken"))
+            if ((mainscript.Instance.TopBorder.transform.position.y - transform.position.y <= 0 && LevelData.mode != ModeGame.Rounded) || (LevelData.mode == ModeGame.Rounded && tag == "chicken"))
             {
                 //Camera.main.GetComponent<mainscript>().controlArray = union(b, Camera.main.GetComponent<mainscript>().controlArray);
                 b.Clear();
