6ef251e0162e9734bb457561b53409ceced54de9
ConnorPeng <pjh_jinghong@outlook.com>
2021-05-09 16:16:33 -0400

    created_startbutton and things

--- Assets/PopSignMain/Scripts/CustomizeLevelScript/CustomizeLevelManager.cs
+++ Assets/PopSignMain/Scripts/CustomizeLevelScript/CustomizeLevelManager.cs
@@ -1,14 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CustomizeLevelManager : MonoBehaviour
 {
     static public CustomizeLevelManager Instance;
     public bool tryingToCustomize = false;
     public HashSet<string> selectedWord = new HashSet<string>();
-    public Dictionary<int, LinkedList<int>> levels = new Dictionary<int, LinkedList<int>>();
+    public Dictionary<int, LinkedList<TextAsset>> levels = new Dictionary<int, LinkedList<TextAsset>>();
 
+    void readlevel() {
+        UnityEngine.Object[] level = Resources.LoadAll("Levels");
+        foreach (UnityEngine.Object temp in level) {
+            TextAsset textAsset = (TextAsset)temp;
+            //Debug.Log(textAsset.text);
+            int index = textAsset.text.IndexOf("COLOR LIMIT");
+            string numstr = textAsset.text.Substring(index + 12, 1);
+            Debug.Log(numstr);
+            int numlevel = Int32.Parse(numstr);
+            if (levels.ContainsKey(numlevel)) {
+                levels[numlevel].AddLast(textAsset);
+            } else {
+                LinkedList<TextAsset> newlist = new LinkedList<TextAsset>();
+                newlist.AddLast(textAsset);
+                levels.Add(numlevel, newlist);
+            }
+        }
+        Debug.Log(levels.Keys);
+        Debug.Log(levels.Values);
+    }
+        
     void Awake()
     {
         if (Instance == null)
@@ -16,6 +38,7 @@ public class CustomizeLevelManager : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(Instance);
             // filter level files and population levels variable
+            readlevel();
         }
     }
     // Start is called before the first frame update
---
+++ Assets/PopSignMain/Scripts/CustomizeLevelScript/startbutton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startbutton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (CustomizeLevelManager.Instance.tryingToCustomize) {
+            this.gameObject.SetActive(true);
+        } else {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
